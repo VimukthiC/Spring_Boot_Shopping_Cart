56e807c5dbb46142495b82cf7eef2298339843c5
vimukthichandrasekera <66357690+vimukthichandrasekera@users.noreply.github.com>
2021-09-16 17:40:24 +0530

    first commit

---
+++ src/main/java/com/wixis360/spring/dto/LineItemDTO.java
@@ -0,0 +1,20 @@
+package com.wixis360.spring.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class LineItemDTO {
+    private int lineItemId;
+    private int quantity;
+    private String orderId;
+    private String itemCode;
+    private String cartId;
+}
---
+++ src/main/java/com/wixis360/spring/service/CartServices.java
@@ -0,0 +1,15 @@
+package com.wixis360.spring.service;
+
+import com.wixis360.spring.dto.CartDTO;
+
+import java.util.List;
+
+public interface CartServices {
+
+
+    boolean saveCart(CartDTO dto);
+    boolean updateCart(CartDTO dto);
+    boolean deleteCart(String id);
+    List<CartDTO> getAllCart();
+    CartDTO searchCart(String id);
+}
